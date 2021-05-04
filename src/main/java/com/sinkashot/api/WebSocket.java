2a2f29f139f1a670b2d6dec2c6c89c20bf3275c0
sinkashot <sinkashot@gmail.com>
2021-05-04 09:41:32 +0900

    websocket 연결 테스트

---
+++ src/main/java/com/sinkashot/api/WebSocket.java
@@ -0,0 +1,51 @@
+package com.sinkashot.api;
+
+import java.util.ArrayList;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@ServerEndpoint("/websocket")
+public class WebSocket {
+	private static ArrayList<Session> sessionList = new ArrayList<Session>();
+	
+	@OnOpen
+	public void handleOpen(Session session) {
+		if (session != null) {
+			String sessionId = session.getId();
+			
+			System.out.println("connected ["+sessionId+"]");
+			sessionList.add(session);
+		}
+	}
+	
+	@OnClose
+	public void handleClose(Session session) {
+		if (session != null) {
+			String sessionId = session.getId();
+			System.out.println("disconnected ["+sessionId+"]");
+		}
+	}
+	
+	@OnError
+	public void handleError(Throwable t) {
+		t.printStackTrace();
+	}
+	
+	@OnMessage
+	public String handleMessage(String message, Session session) {
+		if (session != null) {
+			String sessionId = session.getId();
+			System.out.println("["+sessionId+"] message : "+message);
+		}
+		
+		return null;
+	}
+}
--- src/main/java/com/sinkashot/api/controller/MemberController.java
+++ src/main/java/com/sinkashot/api/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.sinkashot.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +21,11 @@ public class MemberController {
 		return memberService.selectMembers();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping(path = "/members/{number}")
 	public Member findByMemberWithNumber(@PathVariable int number) {
-		return memberService.findByMemberWithNumber(number);
+		Member member = memberService.findByMemberWithNumber(number);
+		System.out.println("findByMemberWithNumber : "+member.getName());
+		return member;
 	}
 }
